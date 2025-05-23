e592c78f8de5273253ad231d4b4b5e73b95c2487
gkratka <gkratka@Guilhermes-MacBook-Air.local>
2025-05-23 16:57:00 -0700

    Updates

--- src/types/modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*';
-declare module './components/Button';
-declare module './components/AttorneyCard';
-declare module './components/ContactForm';
-declare module './data/attorneys'; 
